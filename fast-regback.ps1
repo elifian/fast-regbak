b3b93c346676df0056c5793d715acd5430bb57fb
VasilyVaganov <69606089+vasilyvaganov@users.noreply.github.com>
2024-06-02 04:15:33 +0500

    Update fast-regback.ps1

--- fast-regback.ps1
+++ fast-regback.ps1
@@ -3,48 +3,53 @@ $zipUrl = "https://www.acelogix.com/downloads/regbak.zip"
 $zipPath = "$env:TEMP\regbak.zip"
 $extractPath = "$env:TEMP\regbak"
 $destPath = "$env:windir\RegBak"
-$backupPath = "$destPath\<date>\<time>"
+$dateTime = Get-Date -Format "dd.MM.yy\HH.mm.ss"
+$backupPath = "$destPath\$dateTime"
 $desktopPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("Desktop"), "RegBak.lnk")
-
-# Определение версии файла для распаковки и запуска в зависимости от разрядности ОС
-if ([Environment]::Is64BitOperatingSystem) {
-    $exeFile = "RegBak64.exe"
-} else {
-    $exeFile = "RegBak.exe"
-}
+$regBakExePath = "$destPath\RegBak.exe"
 
 # Создание директории назначения, если она не существует
 if (-Not (Test-Path -Path $destPath)) {
     New-Item -Path $destPath -ItemType Directory -Force
 }
 
-# Загрузка .zip архива
-Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+# Проверка существования RegBak.exe
+if (-Not (Test-Path -Path $regBakExePath)) {
+    # Определение версии файла для распаковки и запуска в зависимости от разрядности ОС
+    if ([Environment]::Is64BitOperatingSystem) {
+        $exeFile = "RegBak64.exe"
+    } else {
+        $exeFile = "RegBak.exe"
+    }
 
-# Распаковка архива
-Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
+    # Загрузка .zip архива
+    Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
 
-# Перенос файла в директорию назначения
-Move-Item -Path "$extractPath\$exeFile" -Destination "$destPath\$exeFile" -Force
+    # Распаковка архива
+    Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
 
-# Переименование файла, если он был RegBak64.exe
-if ($exeFile -eq "RegBak64.exe") {
-    Rename-Item -Path "$destPath\RegBak64.exe" -NewName "RegBak.exe" -Force
+    # Перенос файла в директорию назначения
+    Move-Item -Path "$extractPath\$exeFile" -Destination "$destPath\$exeFile" -Force
+
+    # Переименование файла, если он был RegBak64.exe и если RegBak.exe не существует
+    if ($exeFile -eq "RegBak64.exe" -and -Not (Test-Path -Path "$regBakExePath")) {
+        Rename-Item -Path "$destPath\RegBak64.exe" -NewName "RegBak.exe" -Force
+    }
+
+    # Очистка временных файлов
+    Remove-Item -Path $zipPath -Force
+    Remove-Item -Path $extractPath -Recurse -Force
 }
 
 # Запуск RegBak.exe с указанными аргументами
-Start-Process -FilePath "$destPath\RegBak.exe" -ArgumentList "/dir:`"$backupPath`" /reg:[su] /silent /desc:`"Backup`"" -Wait
+Start-Process -FilePath "$regBakExePath" -ArgumentList "/dir:`"$backupPath`" /reg:[su] /silent /desc:`"Backup`"" -Wait
+
+# Запуск RegBak.exe
+Start-Process -FilePath "$regBakExePath"
 
 # Создание ярлыка на рабочем столе
 $WScriptShell = New-Object -ComObject WScript.Shell
 $shortcut = $WScriptShell.CreateShortcut($desktopPath)
-$shortcut.TargetPath = "$destPath\RegBak.exe"
-$shortcut.IconLocation = "$destPath\RegBak.exe, 0"
+$shortcut.TargetPath = "$regBakExePath"
+$shortcut.IconLocation = "$regBakExePath, 0"
 $shortcut.Save()
-
-# Запуск RegBak.exe
-Start-Process -FilePath "$destPath\RegBak.exe"
-
-# Очистка временных файлов
-Remove-Item -Path $zipPath -Force
-Remove-Item -Path $extractPath -Recurse -Force
